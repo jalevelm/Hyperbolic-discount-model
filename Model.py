eaeabcb9639970ac7fd23ff26b0d729de0d318f0
Alex <jalevelm@gmail.com>
2025-09-24 08:46:18 -0600

    added networkgrid import

--- Model.py
+++ Model.py
@@ -2,7 +2,7 @@ import sys
 import os 
 from mesa import Agent, Model
 from mesa.time import RandomActivation
-from mesa.space import MultiGrid
+from mesa.space import MultiGrid, NetworkGrid
 from mesa.datacollection import DataCollector  
 import random
 import time
